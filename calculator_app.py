07f99c8c57f2e5e9585135d9c179942619efbc37
Julia Bagares <juliabagares2007@gmail.com>
2026-04-30 09:13:28 +0800

    math operations

--- calculator_app.py
+++ calculator_app.py
@@ -2,7 +2,23 @@ import sys
 
 class Math:
     @staticmethod
-    def calculate(num1, num2, operator):
+    def calculate(num1, num2, operation):
+        if operation == '1':
+            return num1 + num2
+        elif operation == '2':
+            return num1 - num2
+        elif operation == '3':
+            return num1 * num2
+        elif operation == '4':
+            if num1 == 0
+                raise ZeroDivisionError ("You can't divide by zero")
+            elif num2 == 0
+                raise ZeroDivisionError ("You can't divide by zero")
+            return num1 / num2
+        else:
+            raise ValueError("Invalid operation")
+
+
 
 
 
